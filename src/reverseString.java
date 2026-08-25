45c3556ac79c87890045fe15f5128a7303caf492
swaraj1216 <swarajbhushan4@gmail.com>
2026-08-25 19:35:08 +0530

    reverse string

---
+++ src/reverseString.java
@@ -0,0 +1,10 @@
+public class reverseString {
+    public static void main(String a[]){
+        String str = "Swaraj";
+        String rev = "";
+        for(int i = str.length()-1; i>=0; i--){
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
