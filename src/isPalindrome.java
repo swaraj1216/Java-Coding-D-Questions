acbc46c718dd3e8b02e400262e9844cc543636dd
swaraj1216 <swarajbhushan4@gmail.com>
2026-08-25 19:35:31 +0530

    palindrome check

---
+++ src/isPalindrome.java
@@ -0,0 +1,13 @@
+public class isPalindrome {
+    public static void main(String a[]){
+        String str = "Nitin";
+        String rev = "";
+        for(int i= str.length()-1; i>=0; i--){
+            rev += str.charAt(i);
+        }
+        String answer = str.equals(rev) ? "Palindrome" : "Not Palindrome"; //case-sensitive
+        String answer2 = str.equalsIgnoreCase(rev) ? "Palindrome" : "Not Palindrome";//case-insensitive
+        System.out.println(answer);
+        System.out.println(answer2);
+    }
+}
