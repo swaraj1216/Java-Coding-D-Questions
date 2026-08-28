6b58b3774072dc27eec706f58f6c71333325f3c8
swaraj1216 <swarajbhushan4@gmail.com>
2026-08-28 23:20:38 +0530

    factorial

---
+++ src/factorial.java
@@ -0,0 +1,10 @@
+public class factorial {
+    public static void main(String args[]){
+        int num = 5;
+        long fact = 1;
+        for(int i =1; i<=num; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial of "+num+" is: "+fact);
+    }
+}
