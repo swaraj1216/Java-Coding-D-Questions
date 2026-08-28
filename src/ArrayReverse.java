02223c3c4f9c9c5a188e093dfab37c81e207a720
swaraj1216 <swarajbhushan4@gmail.com>
2026-08-28 23:24:36 +0530

    reverse array

---
+++ src/ArrayReverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        int start = 0;
+        int end = arr.length -1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println("Reversed array is: "+Arrays.toString(arr));
+    }
+}
